98e51fdecfdc4685f2336e0e9561804b6bc9ef55
DreaSmizz <a.smith102781@gmail.com>
2023-12-04 20:02:21 -0500

    adding week 2

---
+++ Week_2/Monday_Wk2.py
@@ -0,0 +1,55 @@
+# Problem 1 - Rating how my day has been
+day_scale = 7
+"""
+Problem 2 - Describe energy level using a string
+"""
+energy_level = "Mid"
+# Problem 3 - Duration of a song that uplifts you - Tyler Hubbard 'Dancin in the Country'
+song_length = 3.01
+# Problme 4 - Plans to relax after class
+relax = True
+
+#print(song_length)
+print(f"The song length {song_length}!")
+
+
+### Working with Operators ###
+num1 = 3.14
+num2 = 2.53
+#num5 = num1 + num2
+print(num1+num2)
+
+num3 = 4
+num4 = 1
+#num6 = num3 - num4
+print(num3-num4)
+
+"""
+Multiplication and Division Practice
+"""
+num7 = 8
+num8 = 9
+print(num7 * num8)
+
+num10 = 10
+num11 = 5
+print(num10 / num11)
+
+# Calculate the area of rectangle
+length = 4
+width = 10
+area = length * width
+message = "The area of a rectangle is"
+
+print(message, area)
+print(f"The area of a rectangle is {area}")
+
+# Print out your name and age
+name = "Drea" 
+age = 42
+print("My name is", name, "and I am", age, "years of age")
+
+# Greater than, Less than
+num14 = 20
+num15 = 21
+print(num15 > num14)
