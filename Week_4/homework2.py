4a596e72ad1139fcf1b5686c0a3458a7256949a4
DreaSmizz <a.smith102781@gmail.com>
2023-12-18 19:18:57 -0500

    Week 4, Classwork and Homework

---
+++ Week_4/homework2.py
@@ -0,0 +1,21 @@
+#########################
+#Homework Assignment 1  #
+#Andrea Smith           #
+#Family Outing          #
+#v1.0                   #
+#########################
+
+"""
+Ask user if they prefer a park, museum or movie using input()
+"""
+
+outing = input("Would you like to go to the Park, Museum or Movie? ")
+
+if outing.title() == "Park":
+    print("Let's enjoy a day outdoors! ")
+elif outing.title() == "Museum":
+    print("Get ready for a cultural experience! ")
+elif outing.title() == "Movie":
+    print("Movie night it is, good luck agreeing on a selection! ")
+else:
+    print("Please input a valid selection! ")
