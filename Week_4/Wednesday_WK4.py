0b2ab90550736d98a0d48f5b98d2eaa233b236be
DreaSmizz <a.smith102781@gmail.com>
2023-12-20 19:17:26 -0500

    Wednesday Class

--- Week_4/Wednesday_WK4.py
+++ Week_4/Wednesday_WK4.py
@@ -1,31 +1,57 @@
-#Conditional statements - if, else, elif
+#loops - repeat tasks efficiently
+# for loop - when you know in advance how many times you want to loop or iterate through the code
+"""
+for x in reversed(range(1,16)): #reversed() - outputs in the reverse order
+    print(x)
+"""
 
-#if - only executes a block of code if the condition is true
-#else - only executes a block of code if another action or decision because the if statement proved false
-#elif - else if - this precedes the else statement
+#python_cohort = "abcdefghijklmnopqrstuvwxyz"
+
+#for x in python_cohort:
+#    print(x)
 
 """
-age = int(input("Please enter you age: "))
+lucky_number = input("Guess a number: ")
 
-if age < 18:
-    print("You can't vote yet") #indentation - recommendation is 4 spaces
-elif age == 18:
-    print("You are 18, you can vote, CONGRATS.")
+for x in lucky_number:
+    if x == "7":
+        print("You won!")
+    else:
+        print("Sorry you did not win!")
+        """
+"""
+vowel_word = input("Enter a word to search for the vowel u: ")
+
+for letter in vowel_word:
+    if letter == 'u':
+        print("The letter 'u' found in the word.")
+        break #the loop stops instead of continuing to run
+    else:
+        print("There was no 'u' found in the word.")
+        """
+
+#while loop - continue to loop or repeat the block of code while the condition is true
+#guess a secret number and continue to guess until the condition is true
+"""
+#set the secret number
+secretNumber = 2
+#store the user's guess
+userGuess = 0
+#while loop - leep prompting the user
+while userGuess != secretNumber:
+    userGuess = int(input("Guess a secret number between 1 and 10: "))
+    if userGuess == secretNumber:
+        print("Congratulations")
+    else:
+        print("Wrong as raisins in potato salad.  Please try again! ")
+        """
+
+number = 5
+
+while number > 0 and number < 10: #two conditions being checked, joined with and - both of these have to be true for the code to run
+    number = number + 1
+    if number == 7:
+        continue #skip the number and continue running the loop
+    print(number)
 else:
-    print("You are able to vote")
-"""
-
-#logical operators - and, or, not
-# 'and' operator - both statements have to be true
-# 'or' operator - either statement has to be true
-# 'not' operator - evaluates to ensure it is false
-
-age = 25
-income = 5000
-
-#older than 18 and income over 30000
-if age > 18 and income > 30000:
-    print("You are eligible for a loan")
-
-
-
+    print("Number does not meet our conditions")
